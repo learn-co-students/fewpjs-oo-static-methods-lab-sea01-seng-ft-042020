0b121b5f83bce778ea4af8148491c3e550b541c6
aidanmc95 <aidanmc95@gmail.com>
2020-06-10 15:13:42 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
   //add static methods here
+  static capitalize(input) {
+    return input[0].toUpperCase() + input.slice(1)
+  }
+
+  static sanitize(input) {
+    return input.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(input) {
+    const except = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    const arr = input.split(" ")
+    for(let i in arr) {
+      if (!except.includes(arr[i].toLowerCase()) || i == 0) {
+        arr[i] = this.capitalize(arr[i])
+      }
+    }
+    return arr.join(" ")
+  }
 }
