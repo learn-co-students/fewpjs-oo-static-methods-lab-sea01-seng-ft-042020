ef1a1b497c00956f753df9b049bf65ffb5d99377
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-06-09 17:48:33 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.split(" ").map((word) => word.replace(/[^A-Za-z0-9-']+/g, '')).join(" ")
+  }
+
+  static titleize(string) {
+    let filler = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    // Capitalize all not filler words
+    let newString = string.split(" ").map(function(word) {
+      if (!filler.includes(word)) {
+        return Formatter.capitalize(word)
+      } else {
+        return word
+      }
+    }).join(" ")
+    // Capitalize first letter every time
+    return Formatter.capitalize(newString)
+  }
 }
