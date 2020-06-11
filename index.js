bb90b9bbbf1b7c2b3e01cf3a8446e790e16bbf12
johnrusch <johnrusch7@gmail.com>
2020-06-11 13:58:35 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) {
+    let arr = str.split(" ")
+    let newArr = arr.map(word => {return word.replace(/[^A-Za-z0-9-']+/g, '');})
+    return newArr.join(' ');
+  }
+
+  static titleize(str) {
+    let arr = str.toLowerCase().split(' ');
+    let newArr = arr.map(t => {
+      if (t != 'a' && t != 'the' && t != 'an' && t != 'but' && t != 'of' && t != 'and' && t != 'for' && t != 'at' && t != 'by' && t != 'from') {
+        return this.capitalize(t)
+      }
+      return t
+    })
+    return this.capitalize(newArr.join(' '))
+  }
 }
